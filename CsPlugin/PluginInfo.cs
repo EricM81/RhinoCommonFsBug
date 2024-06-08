8f8511ac4e3b1d61a77982e4604e44a61a0e2bf3
Eric M <emarvets@gmail.com>
2024-06-08 17:22:58 -0400

    Add project files.

---
+++ CsPlugin/PluginInfo.cs
@@ -0,0 +1,19 @@
+﻿
+using Rhino.PlugIns;
+
+namespace CsPlugin
+{
+    public class PluginInfo : Rhino.PlugIns.PlugIn
+    {
+        static PlugIn instance;
+
+        public PluginInfo()
+        {
+            instance = this;
+        }
+
+        public static PlugIn Instance => instance;
+
+        public override PlugInLoadTime LoadTime => PlugInLoadTime.AtStartup;
+    }
+}
---
+++ FsPlugin/PluginInfo.fs
@@ -0,0 +1,26 @@
+﻿namespace FsPlugin
+
+open Rhino.PlugIns
+open System.Drawing
+
+// #nowarn "FS1109"
+
+[<AllowNullLiteral>]
+[<System.Runtime.InteropServices.Guid("adf")>]
+type MyPlugin() as this = 
+    inherit PlugIn()
+    static let mutable instance = null
+    
+    do  
+        instance <- this
+    
+    
+    /////<summary>Gets the only instance of the MyRhinoPlugin1Plugin plug-in.</summary>
+    //public static MyRhinoPlugin1Plugin Instance { get; private set; }
+    static member val Instance = instance   
+    
+    
+    //// You can override methods here to change the plug-in behavior on
+    // loading and shut down, add options pages to the Rhino _Option command
+    // and maintain plug-in wide options in a document.
+    override this.LoadTime = PlugInLoadTime.AtStartup
